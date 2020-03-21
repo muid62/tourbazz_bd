c2e486653fe3b6ddc8f398330e049262a582a4f7
shamsujohasumon <38026251+shamsujohasumon@users.noreply.github.com>
2020-03-21 11:42:29 +0600

    adding media

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import hotels
+from .views import hotels,single_hotel
 
 
 urlpatterns = [
    path('hotels',hotels,name='hotels'),
+   path('single_hotel/<int:hotel_id>',single_hotel,name='single_hotel'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Hotel
 
 def hotels(request):
-    return render(request,'hotel/hotels.html')
+    listings = Hotel.objects.all()
+    context = {
+        'listings':listings
+    }
+    return render(request,'hotel/hotels.html',context)
+
+def single_hotel(request,hotel_id):
+    return render(request,'hotel/single_hotel.html')
