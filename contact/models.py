ed31f11420f45916a63e47d36647f56cfdc30478
shamsujohasumon <38026251+shamsujohasumon@users.noreply.github.com>
2020-03-23 13:16:14 +0600

    last update

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    subject = models.CharField(max_length=100)
+    message = models.CharField(max_length=1500)
+
+    def __str__(self):
+        return self.name
+
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Person
 
 def contact(request):
+    if request.method=='POST':
+        muid = Person(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            subject=request.POST['subject'],
+            message=request.POST['message']
+        )
+        muid.save()
+
     return render(request,'contact/contact.html')
 
 def about(request):
