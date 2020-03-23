4e9937e9e7934053914acdaf517b7af3d15e343c
shamsujohasumon <38026251+shamsujohasumon@users.noreply.github.com>
2020-03-23 15:16:26 +0600

    only test folder

--- README.md
+++
@@ -1,1 +0,0 @@
-# tourbazz_bd
--- contact/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def contact(request):
-    return render(request,'contact/contact.html')
-
-def about(request):
-    return render(request,'contact/about.html')
