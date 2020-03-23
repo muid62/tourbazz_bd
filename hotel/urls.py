146072ac206482ab1f85d72c7bc8e7350f912224
md. abdullah al muid <md.abdullahalmuid>
2020-03-23 13:55:04 +0600

    all file delete

--- hotel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import hotels,single_hotel
-
-
-urlpatterns = [
-   path('hotels',hotels,name='hotels'),
-   path('single_hotel/<int:hotel_id>',single_hotel,name='single_hotel'),
-]
